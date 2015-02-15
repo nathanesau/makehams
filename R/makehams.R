1d46f0f5c73de88c4c5909106e9a0586321b4bc3
Nathan Esau <nathanesau1@gmail.com>
2015-02-15 13:52:16 -0500

    fixed compile errors

--- R/makehams.R
+++ R/makehams.R
@@ -1,3 +1,9 @@
+#' @title globalParameters
+#' @description Contains all of the global parameters used by the Makeham Model. 
+#'              "A","B","c" are makeham model params, "d" is select period, "x" is default age, 
+#'              "w" is limiting age, "radix" is used as initial survivors for life tables, 
+#'              "i" is the effective annual interest rate.
+#' @export
 global = list(A=0.00022, B=2.7e-06, c=1.124, d=2, x=20, w=131, radix=1e+05, i=0.05) 
 
 #' @title updateParams
@@ -10,6 +16,6 @@ global = list(A=0.00022, B=2.7e-06, c=1.124, d=2, x=20, w=131, radix=1e+05, i=0.
 #' @export
 updateParams <- function(paramNames,paramVals) {
   for(i in 1:length(paramNames)) {
-    eval(parse(text=paste(paste("global",paramNames[i],sep="$"),"<<-",toString(paramVals[i]),sep="")))
+    eval(parse(text=paste(paste("global",paramNames[i],sep="$"),"<-",toString(paramVals[i]),sep="")))
   }
 }
